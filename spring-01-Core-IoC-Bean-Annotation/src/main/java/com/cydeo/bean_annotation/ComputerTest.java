b9440a1611047245cbba764771232c23b7d29df5
Adam Husrew <Adam.Husrew@cydeo.com>
2022-12-31 23:36:13 +0300

    Initial commit

---
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -0,0 +1,26 @@
+package com.cydeo.bean_annotation;
+
+import com.cydeo.bean_annotation.casefactory.Case;
+import com.cydeo.bean_annotation.config.ComputerConfig;
+import com.cydeo.bean_annotation.monitorfactory.Monitor;
+import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class ComputerTest {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ComputerConfig.class);
+
+        Monitor theMonitor = container.getBean(Monitor.class);
+        Case theCase = container.getBean(Case.class);
+        Motherboard theMotherBoard = container.getBean(Motherboard.class);
+
+        PC myPc = new PC(theCase, theMonitor, theMotherBoard);
+        myPc.powerUp();
+
+
+    }
+}
