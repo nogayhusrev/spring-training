f5ab84d775e7267c57bcc5ba44acb463b0f5c6f7
Adam Husrew <Adam.Husrew@cydeo.com>
2023-01-03 17:03:03 +0300

    @Primary

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -4,6 +4,7 @@ import com.cydeo.bean_annotation.casefactory.Case;
 import com.cydeo.bean_annotation.config.ComputerConfig;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,14 +12,40 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class ComputerTest {
 
     public static void main(String[] args) {
-
+//      Creating container by using Application Context
         ApplicationContext container = new AnnotationConfigApplicationContext(ComputerConfig.class);
 
+//      Creating container by using BeanFactory
+        BeanFactory context = new AnnotationConfigApplicationContext(ComputerConfig.class);
+
+
+
+        System.out.println("********************Retrieving Beans******************");
+/*
         Monitor theMonitor = container.getBean(Monitor.class);
         Case theCase = container.getBean(Case.class);
         Motherboard theMotherBoard = container.getBean(Motherboard.class);
 
+
         PC myPc = new PC(theCase, theMonitor, theMotherBoard);
+        myPc.powerUp();
+
+ */
+        System.out.println();
+
+
+        System.out.println("********************Multiple Object******************");
+        Monitor theMonitor2 = container.getBean("monitorSony", Monitor.class); //Default bean name
+        Monitor theMonitor3 = container.getBean("sony", Monitor.class); //Custom bean name
+        Monitor theMonitor4 = container.getBean( Monitor.class); //@Primary
+
+
+
+
+        Case theCase = container.getBean(Case.class);
+        Motherboard theMotherBoard = container.getBean(Motherboard.class);
+
+        PC myPc = new PC(theCase, theMonitor2, theMotherBoard);
         myPc.powerUp();
 
 
--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
@@ -2,12 +2,14 @@ package com.cydeo.bean_annotation.config;
 
 import com.cydeo.bean_annotation.casefactory.Case;
 import com.cydeo.bean_annotation.casefactory.DellCase;
+import com.cydeo.bean_annotation.monitorfactory.AcerMonitor;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
 import com.cydeo.bean_annotation.motherboardfactory.AsusMotherboard;
 import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ComputerConfig {
@@ -17,6 +19,10 @@ public class ComputerConfig {
         return new SonyMonitor("34 inch Beast", "Sony", 34);
     }
 
+    @Bean
+    @Primary
+    public Monitor monitorAcer(){ return new AcerMonitor("23 inch Beast","Acer",34);
+    }
     @Bean
     public Case caseDell() {
         return new DellCase("220B", "Dell", "240");
