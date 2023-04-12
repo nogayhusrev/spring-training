02e5472a6a0ebabfcdaacb09b0ef76496a4c5ed1
nogayhusrev <nogayhusrev@gmail.com>
2023-04-12 05:03:08 +0300

    Spring day 08 spring-mvc-intro done

---
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String getHomePage() {
+        return "home.html";
+    }
+
+    @RequestMapping("/husrev")
+    public String getHomePage2() {
+        return "home.html";
+    }
+
+    @RequestMapping // nothing and /
+    public String getHomePage3() {
+        return "home.html";
+    }
+
+    @RequestMapping({"/apple", "/orange"})
+    public String getHomePage4() {
+        return "home.html";
+    }
+
+
+}
