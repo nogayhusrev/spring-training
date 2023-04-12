5f84160ae02c5962f6d8f7846b8a8bd46f57fbdf
nogayhusrev <nogayhusrev@gmail.com>
2023-04-12 04:07:15 +0300

    Spring day 05 spring-core  bean-scope done and spring core done

---
+++ spring-06-Core-BeanScope/src/main/java/com/nogayhusrev/config/CommentConfig.java
@@ -0,0 +1,11 @@
+package com.nogayhusrev.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.nogayhusrev")
+//@ComponentScan(basePackages = {"com.nogayhusrev.proxy","com.nogayhusrev.service","com.nogayhusrev.repository"})
+public class CommentConfig {
+
+}
---
+++ spring-06-Core-BeanScope/src/main/java/com/nogayhusrev/proxy/PushCommentNotificationProxy.java
@@ -0,0 +1,15 @@
+package com.nogayhusrev.proxy;
+
+import com.nogayhusrev.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("PUSH")
+public class PushCommentNotificationProxy implements CommentNotificationProxy {
+
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending push notification for comment:" + comment.getText());
+    }
+}
