98e59f2ee0599cee332b7bc013ca95b603cce11d
nogayhusrev <nogayhusrev@gmail.com>
2023-04-12 05:18:06 +0300

    Spring day 10 spring-mvc Thymeleaf done

---
+++ spring-10-Thymeleaf/src/main/java/com/nogayhusrev/Spring10ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.nogayhusrev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring10ThymeleafApplication.class, args);
+    }
+
+}
