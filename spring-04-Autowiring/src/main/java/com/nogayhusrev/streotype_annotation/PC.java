5bcb9b2f3731a7f0b4ba6bb6201d2c041ac05fb5
nogayhusrev <nogayhusrev@gmail.com>
2023-04-12 03:57:46 +0300

    Spring day 04 autowiring done

---
+++ spring-04-Autowiring/src/main/java/com/nogayhusrev/streotype_annotation/PC.java
@@ -0,0 +1,34 @@
+package com.nogayhusrev.streotype_annotation;
+
+import com.nogayhusrev.streotype_annotation.casefactory.Case;
+import com.nogayhusrev.streotype_annotation.monitorfactory.Monitor;
+import com.nogayhusrev.streotype_annotation.motherboardfactory.Motherboard;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+        // Fancy graphics
+        monitor.drawPixelAt();
+    }
+
+}
