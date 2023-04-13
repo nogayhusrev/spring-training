5d281798e98b8d23ea373fe3d404081bfadd4468
nogayhusrev <nogayhusrev@gmail.com>
2023-04-14 02:06:55 +0300

    Spring Day 18 Rest SchoolLab WeatherStack dtos added.

---
+++ spring-18-REST-SchoolLab/src/main/java/com/nogayhusrev/client/WeatherApiClient.java
@@ -0,0 +1,14 @@
+package com.nogayhusrev.client;
+
+
+import com.nogayhusrev.dto.WeatherDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(url = "http://api.weatherstack.com", name = "WEATHER-CLIENT")
+public interface WeatherApiClient {
+
+    @GetMapping("/current")
+    WeatherDTO getCurrentWeather(@RequestParam(value = "access_key") String key, @RequestParam(value = "query") String city);
+ }
