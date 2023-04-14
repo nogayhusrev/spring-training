be6664ddc3436d98dc269527f57e9726f27e053b
nogayhusrev <nogayhusrev@gmail.com>
2023-04-15 01:35:41 +0300

    Spring Day 19 Rest OpenAPI 3 done. Yaml file format done

---
+++ spring-19-REST-OpenAPI3/src/main/java/com/nogayhusrev/service/UserService.java
@@ -0,0 +1,13 @@
+package com.nogayhusrev.service;
+
+import com.nogayhusrev.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UserDTO> getUsers();
+
+    UserDTO create(UserDTO userDTO);
+
+}
