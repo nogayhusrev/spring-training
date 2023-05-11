d3ea0988ee3a8067ae05641d987f824ae90c2668
nogayhusrev <nogayhusrev@gmail.com>
2023-05-11 15:26:37 +0300

    Spring Day 25 docker done

---
+++ spring-25-Docker/src/test/java/com/nogayhusrev/Spring25DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nogayhusrev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring25DockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
